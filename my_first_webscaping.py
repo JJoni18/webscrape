8d16006187458c3cafac3fbe9b9d490657035e79
John <onijohn1025@gmail.com>
2021-01-10 18:32:56 -0500

    update

---
+++ my_first_webscaping.py
@@ -0,0 +1,29 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup
+
+my_url = 'https://www.newegg.com/p/pl?d=ddr3+server+RAM'
+
+#opening up connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#html parsing
+page_soup = BeautifulSoup(page_html, "html.parser")
+
+#grabs each product
+ containers = page_soup.findAll("div",{"class":"item-container"})
+
+for container in containers:
+    brand = container.div.div.a.img["title"]
+
+    title_container = container.findAll("a", {"class":"item-title"})
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()
+
+    print("brand: " + brand)
+    print("product_name: " + product_name)
+    print("shipping: " + shipping)
