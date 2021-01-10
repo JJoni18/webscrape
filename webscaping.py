caaf54c821e862ba1d8910f94ab0484c2c462c70
John <onijohn1025@gmail.com>
2021-01-10 16:58:38 -0500

    update

--- webscaping.py
+++ webscaping.py
@@ -1,3 +1,10 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
+
+my_url = 'https://www.newegg.com/p/pl?d=ddr3+server+RAM'
+
+#opening up connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
