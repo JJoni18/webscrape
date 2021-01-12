8dc6a49f884fbc699d7938a4825c08db12dbb9f6
John Oni <onijohn1025@gmail.com>
2021-01-11 20:39:56 -0500

    update

--- phones-emails.py
+++ phones-emails.py
@@ -6,7 +6,7 @@ data = requests.get(input("enter a url: "))
 
 #extract the phone numbers, emails, and e.t.class
 
-phones = re.findall(r'(\(?[0-9{3}\)?(?:\-|\s|\.)?[0-9]{3}(?:\-|\.)[0-9]{4})', data.text)
+phones = re.findall(r'(\(?[0-9]{3}\)?(?:\-|\s|\.)?[0-9]{3}(?:\-|\.)[0-9]{4})', data.text)
 emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
 
 print(phones, emails)
