a6d586a48613e77e9df73684e9fd18be2ffe033a
John <onijohn1025@gmail.com>
2021-01-19 00:18:45 -0500

    indeed scrape

--- webscraping-indeed.py
+++ webscraping-indeed.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def extract(page):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
@@ -13,7 +14,30 @@ def transform(soup):
     for item in divs:
         title = item.find('a').text.strip()
         company = item.find('span', class_ ='company').text.strip()
-        print(company)
+        try:
+            salary = item.find('span', class_ = 'salaryText').text.strip()
+        except:
+            salary = ''
+        summary = item.find('div', {'class' : 'summary'}).text.strip().replace('\n', '')
 
-c = extract(0)
-transform(c)
+
+       #job dictionary
+        job = {
+            'titel': title,
+            'company': company,
+            'salary': salary,
+            'summary': summary
+        }
+        joblist.append(job)
+    return
+
+joblist = []
+
+for i in range(0,40,10):
+    print(f'Getting page, {i}')
+    c = extract(0)
+    transform(c)
+
+df = pd.DataFrame(joblist)
+print(df.head())
+df.to_csv('jobs.csv')
